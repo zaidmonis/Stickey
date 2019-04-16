53f881ceaaf34238cf284983abb41544586d7905
zaid <monis.z.786@gmail.com>
2019-04-16 20:42:06 +0530

    Initial commit

---
+++ app/src/main/java/thezaxis/stickey/BaseActivity.java
@@ -0,0 +1,83 @@
+/*
+ * Copyright (c) WhatsApp Inc. and its affiliates.
+ * All rights reserved.
+ *
+ * This source code is licensed under the BSD-style license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
+package thezaxis.stickey;
+
+import android.app.Dialog;
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.StringRes;
+import android.support.v4.app.DialogFragment;
+import android.support.v7.app.AlertDialog;
+import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+
+public abstract class BaseActivity extends AppCompatActivity {
+    @Override
+    public boolean onSupportNavigateUp() {
+        onBackPressed();
+        return true;
+    }
+
+    public static final class MessageDialogFragment extends DialogFragment {
+        private static final String ARG_TITLE_ID = "title_id";
+        private static final String ARG_MESSAGE = "message";
+
+        public static DialogFragment newInstance(@StringRes int titleId, String message) {
+            DialogFragment fragment = new MessageDialogFragment();
+            Bundle arguments = new Bundle();
+            arguments.putInt(ARG_TITLE_ID, titleId);
+            arguments.putString(ARG_MESSAGE, message);
+            fragment.setArguments(arguments);
+            return fragment;
+        }
+
+        @NonNull
+        @Override
+        public Dialog onCreateDialog(Bundle savedInstanceState) {
+            @StringRes final int title = getArguments().getInt(ARG_TITLE_ID);
+            String message = getArguments().getString(ARG_MESSAGE);
+
+            AlertDialog.Builder dialogBuilder = new AlertDialog.Builder(getActivity())
+                    .setMessage(message)
+                    .setCancelable(true)
+                    .setPositiveButton(android.R.string.ok, (dialog, which) -> dismiss());
+
+            if (title != 0) {
+                dialogBuilder.setTitle(title);
+            }
+            return dialogBuilder.create();
+        }
+    }
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater menuInflater = getMenuInflater();
+        menuInflater.inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()){
+            case R.id.action_other_apps:{
+                showdeveloperPage();
+            }
+        }
+        return true;
+    }
+
+    private void showdeveloperPage() {
+        Intent developerPageIntent = new Intent(Intent.ACTION_VIEW);
+        developerPageIntent.setData(Uri.parse("https://play.google.com/store/apps/dev?id=8144597860499618446"));
+        startActivity(developerPageIntent);
+    }
+}
